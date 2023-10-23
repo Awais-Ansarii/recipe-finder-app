1f825c2794fd3d6cfa4e6fdfca0bc10f5642596e
Awais-Ansarii <awaisansari1502@outlook.com>
2023-10-23 23:14:25 +0530

    fetchted data via API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-
+import { useState, useEffect } from "react";
+import { App_Id, App_Key } from "./constants";
+import axios from "axios";
 
 function App() {
+  
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
+
+ 
+  const getRecipe = async () => {
+    const response = await axios.get(
+      `https://api.edamam.com/search?q=${query}&app_id=${App_Id}&app_key=${App_Key}`
+    );
+    setRecipes(response.data.hits);
+    console.log(response.data.hits); 
+  };
+
+  
+
+  useEffect(() => {
+    getRecipe();
+  }, []);
+
+ 
+
   return (
     <div className="App">
         <form >
           <input type="text"/>
           <button>search</button>
         </form>
+       
     </div>
   );
 }
